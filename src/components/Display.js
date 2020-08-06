2a3f304318371c1ca33f4acefab7c51bc4595228
Carlos Teller <carlos.z.teller@gmail.com>
2020-08-05 19:00:07 -0500

    Added fetching demo videos from unicodeveloper Cloudinary

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from './Nav';
-import { Container, Typography } from '@material-ui/core';
+import { Container, Typography, Grid } from '@material-ui/core';
 import { isLoggedIn } from '../utils/AuthService';
 import axios from 'axios';
+import { CloudinaryContext, Transformation, Video } from 'cloudinary-react';
 
 const Display = () => {
+    const [videos, setVideos] = useState([]);
+
+    const getVideos = () => {
+        axios.get('http://res.cloudinary.com/unicodeveloper/video/list/miniflix.json')
+            .then(res => {
+                console.log(res.data.resources);
+                
+                setVideos(res.data.resources);
+            });
+    }
+
+    useEffect(() => {
+        getVideos();
+    });
+
     return (
         <div>
             <Nav />
@@ -17,6 +33,17 @@ const Display = () => {
                 >
                     Latest Videos on Miniflix
                 </Typography>
+
+                <CloudinaryContext cloudName="unicodeveloper">
+                    <Grid container spacing={3}>
+                        {videos.map((video, i) => (
+                            <Grid item key={video.public_id} xs={12} sm={6} md={3}>
+                                <Video publicId={video.public_id} className="video" controls></Video>
+                                <Typography variant="body2">Created at: {video.created_at}</Typography>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </CloudinaryContext>
             </Container>
         </div>
     );
